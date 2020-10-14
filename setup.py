e8534e64197541a23fe2b18cb34981c8f5d79f39
Ryan Lanny Jenkins <rjenkins@styleseat.com>
2020-10-13 21:40:56 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='ss_instrumentation',
+      version='0.1',
+      description='',
+      url='https://github.com/styleseat/ss-instrumentation',
+      author='Some Dude at StyleSeat',
+      packages=['ss_instrumentation'],
+      install_requires = [
+          'boto3==1.15.16',
+      ],
+      zip_safe=True)
+
