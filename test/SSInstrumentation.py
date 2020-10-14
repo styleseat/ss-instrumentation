cd6d2cdb169bff09179830f5d74d6b853051111e
Ryan Lanny Jenkins <rjenkins@styleseat.com>
2020-10-13 22:55:59 -0500

    Got things tests working with tox

--- test/SSInstrumentation.py
+++ test/SSInstrumentation.py
@@ -31,6 +31,23 @@ class TestSSInstrumenation(object):
 
         return SSInstrumentation(config)
 
+    def _metric_ident(self, metric):
+        dims = sorted([(dim['Name'], dim['Value']) for dim in metric.get('Dimensions', [])])
+        return (metric['MetricName'], tuple(dims))
+
+    def assert_metric_present(self, actual_metrics, expected_metric):
+        actual_metrics = {self._metric_ident(metric): metric for metric in actual_metrics}
+        metric_ident = self._metric_ident(expected_metric)
+
+        if metric_ident not in actual_metrics:
+            assert 1 == 0, 'Metric with matching name/dims not found'
+
+        actual_metric = actual_metrics[metric_ident]
+
+        assert actual_metric.get('Value') == expected_metric.get('Value')
+        assert actual_metric.get('Unit') == expected_metric.get('Unit')
+        assert actual_metric.get('Timestamp') == expected_metric.get('Timestamp')
+
     @mock.patch('boto3.client')
     def test_client_config(self, mock_client_constructor):
         self.create_instr()
@@ -58,26 +75,24 @@ class TestSSInstrumenation(object):
         instr = self.create_instr()
         instr.put_metric('fizz', 6, is_prime='no', why='divisible by two')
 
-        mock_client.put_metric_data.assert_called_with(
-            Namespace='FizzBuzzAsAService',
-            MetricData=[
-                {
-                    'MetricName': 'fizz',
-                    'Value': 6,
-                    'Unit': 'None',
-                    'Timestamp': datetime.utcnow(),
-                    'Dimensions': [
-                        {
-                            'Name': 'is_prime',
-                            'Value': 'no',
-                        },
-                        {
-                            'Name': 'why',
-                            'Value': 'divisible by two',
-                        },
-                    ]
-                }
-            ]
+        self.assert_metric_present(
+            mock_client.put_metric_data.call_args.kwargs['MetricData'],
+            {
+                'MetricName': 'fizz',
+                'Value': 6,
+                'Unit': 'None',
+                'Timestamp': datetime.utcnow(),
+                'Dimensions': [
+                    {
+                        'Name': 'is_prime',
+                        'Value': 'no',
+                    },
+                    {
+                        'Name': 'why',
+                        'Value': 'divisible by two',
+                    },
+                ]
+            }
         )
 
     @standard_mock
@@ -126,32 +141,34 @@ class TestSSInstrumenation(object):
 
             instr.flush_meters()
 
-            mock_client.put_metric_data.assert_called_with(
-                Namespace='FizzBuzzAsAService',
-                MetricData=[
-                    {
-                        'MetricName': 'bps',
-                        'Value': 0.5,
-                        'Unit': 'Count/Second',
-                        'Timestamp': datetime.utcnow(),
-                        'Dimensions': [
-                            {
-                                'Name': 'is_prime',
-                                'Value': 'yes',
-                            },
-                        ],
-                    },
-                    {
-                        'MetricName': 'bps',
-                        'Value': 1,
-                        'Unit': 'Count/Second',
-                        'Timestamp': datetime.utcnow(),
-                        'Dimensions': [
-                            {
-                                'Name': 'is_prime',
-                                'Value': 'no',
-                            },
-                        ],
-                    },
-                ]
+            self.assert_metric_present(
+                mock_client.put_metric_data.call_args.kwargs['MetricData'],
+                {
+                    'MetricName': 'bps',
+                    'Value': 0.5,
+                    'Unit': 'Count/Second',
+                    'Timestamp': datetime.utcnow(),
+                    'Dimensions': [
+                        {
+                            'Name': 'is_prime',
+                            'Value': 'yes',
+                        },
+                    ],
+                }
+            )
+
+            self.assert_metric_present(
+                mock_client.put_metric_data.call_args.kwargs['MetricData'],
+                {
+                    'MetricName': 'bps',
+                    'Value': 1,
+                    'Unit': 'Count/Second',
+                    'Timestamp': datetime.utcnow(),
+                    'Dimensions': [
+                        {
+                            'Name': 'is_prime',
+                            'Value': 'no',
+                        },
+                    ],
+                },
             )
