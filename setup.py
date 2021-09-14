fda501206c98fd9585d7d128bfb52f6629f47540
Elijah Shaw-Rutschman <elijahr@gmail.com>
2021-09-14 13:17:24 -0500

    Fix linter issues

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import (
     setup,
 )
 
-setup(name='ss_instrumentation',
-      version='2.0.0',
-      description='',
-      url='https://github.com/styleseat/ss-instrumentation',
-      author='Some Dude at StyleSeat',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      install_requires=['boto3>1'],
-      zip_safe=True)
-
+setup(
+    name="ss_instrumentation",
+    version="2.0.0",
+    description="",
+    url="https://github.com/styleseat/ss-instrumentation",
+    author="Some Dude at StyleSeat",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=["boto3>1"],
+    zip_safe=True,
+)
