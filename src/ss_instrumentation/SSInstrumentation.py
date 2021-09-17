b7e734a49c765ee8c7c61a0c78311b6e60de3ff9
Elijah Shaw-Rutschman <elijahr@gmail.com>
2021-09-17 13:53:03 -0500

    Ensure src is linted

--- src/ss_instrumentation/SSInstrumentation.py
+++ src/ss_instrumentation/SSInstrumentation.py
@@ -1,35 +1,42 @@
-from __future__ import absolute_import
+from __future__ import (
+    absolute_import,
+)
 
 import json
 import time
-from abc import ABCMeta, abstractmethod
-from datetime import datetime
-from collections import defaultdict
+from abc import (
+    ABCMeta,
+    abstractmethod,
+)
+from collections import (
+    defaultdict,
+)
+from datetime import (
+    datetime,
+)
 
 import boto3
-from botocore.client import Config
+from botocore.client import (
+    Config,
+)
 
 
 def batch(iterable, n=1):
     count = len(iterable)
     for ndx in range(0, count, n):
-        yield iterable[ndx:min(ndx + n, count)]
+        yield iterable[ndx : min(ndx + n, count)]
 
 
 class MetricAndDims(object):
-    """
-    Hashable and serializable storage for a metric name and associated
-    dimension name/value pairs.
-    """
+    """Hashable and serializable storage for a metric name and associated
+    dimension name/value pairs."""
+
     def __init__(self, name, **kwargs):
         self.name = name
         self.dims = kwargs
 
     def serialize(self):
-        return json.dumps([
-            self.name,
-            sorted([(k, v) for k, v in self.dims.items()])
-        ])
+        return json.dumps([self.name, sorted([(k, v) for k, v in self.dims.items()])])
 
     @classmethod
     def deserialize(cls, s):
@@ -86,38 +93,41 @@ class RedisMetricStorage(MetricStorage):
     def __init__(self, redis_client):
         self._client = redis_client
 
-        if not self._client.exists('ssinstr:meter:periodstart'):
-            self._client.set('ssinstr:meter:periodstart', time.time())
+        if not self._client.exists("ssinstr:meter:periodstart"):
+            self._client.set("ssinstr:meter:periodstart", time.time())
 
     def incr(self, metric):
-        self._client.hincrby('ssinstr:meter:counts', metric.serialize(), 1)
+        self._client.hincrby("ssinstr:meter:counts", metric.serialize(), 1)
 
     def pop_values_for_period(self):
-        duration = time.time() - float(self._client.get('ssinstr:meter:periodstart'))
-        raw_counts = self._client.hgetall('ssinstr:meter:counts')
+        duration = time.time() - float(self._client.get("ssinstr:meter:periodstart"))
+        raw_counts = self._client.hgetall("ssinstr:meter:counts")
         values = {}
 
         for metric_str, count in raw_counts.items():
             metric = MetricAndDims.deserialize(metric_str)
             values[metric] = float(count) / duration
 
-        self._client.set('ssinstr:meter:periodstart', time.time())
-        self._client.delete('ssinstr:meter:counts')
+        self._client.set("ssinstr:meter:periodstart", time.time())
+        self._client.delete("ssinstr:meter:counts")
 
         return values
 
 
 class SSInstrumentation(object):
-    """Class for managing methods that send data to be stored in AWS"""
+    """Class for managing methods that send data to be stored in AWS."""
 
     def __init__(self, config, storage=None):
-        self.namespace = config['AWS_METRIC_NAMESPACE']
-        self.client = boto3.client('cloudwatch', config=Config(
-            connect_timeout=config.get('BOTO_CONNECT_TIMEOUT', 1),
-            read_timeout=config.get('BOTO_READ_TIMEOUT', 1),
-            retries={'max_attempts': 0},
-            region_name=config['AWS_LOGGING_REGION']
-        ))
+        self.namespace = config["AWS_METRIC_NAMESPACE"]
+        self.client = boto3.client(
+            "cloudwatch",
+            config=Config(
+                connect_timeout=config.get("BOTO_CONNECT_TIMEOUT", 1),
+                read_timeout=config.get("BOTO_READ_TIMEOUT", 1),
+                retries={"max_attempts": 0},
+                region_name=config["AWS_LOGGING_REGION"],
+            ),
+        )
 
         if storage:
             self._storage = storage
@@ -131,42 +141,38 @@ class SSInstrumentation(object):
 
     def put_metric(self, metric_name, value, **kwargs):
         data = {
-            'name': metric_name,
-            'value': value,
+            "name": metric_name,
+            "value": value,
         }
 
         if kwargs:
-            data['dims'] = kwargs
+            data["dims"] = kwargs
 
         return self.put_metrics([data])
 
     def put_metrics(self, metrics):
-        """
-        Store multiple metrics in CloudWatch. May fail in various ways, returns
-        True on success but in most contexts you won't want to retry or even
-        bother to check the return value.
+        """Store multiple metrics in CloudWatch.
+
+        May fail in various ways, returns True on success but in most
+        contexts you won't want to retry or even bother to check the
+        return value.
         """
         metrics_data = []
         for metric in metrics:
             metric_data = {
-                'MetricName': metric['name'],
-                'Value': metric.get('value', 1),
-                'Unit': metric.get('unit', 'None'),
-                'Timestamp': datetime.utcnow()
+                "MetricName": metric["name"],
+                "Value": metric.get("value", 1),
+                "Unit": metric.get("unit", "None"),
+                "Timestamp": datetime.utcnow(),
             }
 
-            if 'dims' in metric:
-                metric_data['Dimensions'] = [
-                    {'Name': name, 'Value': value} for name, value in metric['dims'].items()
-                ]
+            if "dims" in metric:
+                metric_data["Dimensions"] = [{"Name": name, "Value": value} for name, value in metric["dims"].items()]
 
             metrics_data.append(metric_data)
 
         try:
-            self.client.put_metric_data(
-                Namespace=self.namespace,
-                MetricData=metrics_data
-            )
+            self.client.put_metric_data(Namespace=self.namespace, MetricData=metrics_data)
             return True
         except Exception:
             return False
@@ -181,11 +187,13 @@ class SSInstrumentation(object):
             metrics_to_put = []
 
             for metric, rate in metric_batch:
-                metrics_to_put.append({
-                    'name': metric.name,
-                    'dims': metric.dims,
-                    'value': rate,
-                    'unit': 'Count/Second',
-                })
+                metrics_to_put.append(
+                    {
+                        "name": metric.name,
+                        "dims": metric.dims,
+                        "value": rate,
+                        "unit": "Count/Second",
+                    }
+                )
 
             self.put_metrics(metrics_to_put)
